8a22d8b0649a88279c7ba7b9ac3b9be02c6585c2
Bas van den Heuvl <basvandenheuvel@live.nl>
2013-05-23 20:15:05 +0200

    docent detail pagina gefixt
    deze haalt nu officele data uit database : naam pagina is
    instructordetail.cs

--- Trainee_Manager/Trainee_Manager/View/InstructorDetails.xaml.cs
+++ Trainee_Manager/Trainee_Manager/View/InstructorDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Trainee_Manager.Controller;
 
 namespace Trainee_Manager.View
 {
@@ -23,11 +25,34 @@ namespace Trainee_Manager.View
         //unique id of the instructor record in the database table "docenten"
         private int id;
 
+        private static DataTable dataTable;
+
         public InstructorDetails(int id)
         {
             InitializeComponent();
 
             this.id = id;
+
+            getData();
+        }
+
+        private void getData()
+        {
+
+            //Call the procedure to load the mysql data
+            dataTable = DatabaseConnection.commandSelect("CALL procedure_docent_details(" + id + ");");
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                textbox_naam.Text = row["naam"].ToString();
+                textbox_email.Text = row["email"].ToString();
+                textbox_kamernummer.Text = row["kamernummer"].ToString();
+                textbox_telefoonnummer.Text = row["telefoonnummer"].ToString();
+                textbox_adres.Text = row["adres"].ToString();
+                textbox_postcode.Text = row["postcode"].ToString();
+                textbox_plaats.Text = row["plaats"].ToString();
+                textbox_telefoonnummer_prive.Text = row["telefoonnummer_prive"].ToString();                
+            }
         }
     }
 }
