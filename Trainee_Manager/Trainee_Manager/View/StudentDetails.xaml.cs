4c92b8d5bda8eb19ce81f694b5510095bbba9f99
Bas van den Heuvl <basvandenheuvel@live.nl>
2013-05-24 11:12:13 +0200

    studentdetail pagina gefixt

--- Trainee_Manager/Trainee_Manager/View/StudentDetails.xaml.cs
+++ Trainee_Manager/Trainee_Manager/View/StudentDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Trainee_Manager.Controller;
 
 namespace Trainee_Manager.View
 {
@@ -22,11 +24,39 @@ namespace Trainee_Manager.View
     {
         private int id;
 
+        private static DataTable dataTable;
+
         public StudentDetails(int id)
         {
             InitializeComponent();
 
             this.id = id;
+            getData();
+        }
+
+        private void getData()
+        {
+
+            //Call the procedure to load the mysql data
+            dataTable = DatabaseConnection.commandSelect("CALL procedure_student_details(" + id + ");");
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                //HIERONDER VERDER AF MAKEN
+                textbox_studentnummer.Text = row["studentnr"].ToString();
+                textbox_studentcode.Text = row["studentcode"].ToString();
+                textbox_achternaam.Text = row["achternaam"].ToString();
+                textbox_voorletters.Text = row["voorvoegsels"].ToString();
+                textbox_roepnaam.Text = row["roepnaam"].ToString();
+                textbox_email.Text = row["email"].ToString();
+                textbox_straat.Text = row["straatnaam"].ToString();
+                textbox_huisnummer.Text = row["nummer"].ToString();
+                textbox_huistoevoeging.Text = row["toevoeging"].ToString();
+                textbox_postcode.Text = row["postcode"].ToString();
+                textbox_plaats.Text = row["plaats"].ToString();
+                textbox_telefoonnummer.Text = row["telefoonnummer"].ToString();
+                textbox_opmerking.Text = row["opmerking"].ToString();
+            }
         }
     }
 }
