989db979c25193ead05c9dd167d9945ea12514e9
Pim Verlangen <pim.verlangen@gmail.com>
2013-05-26 20:48:03 +0200

    Excel Prototype added (ExcelTest)
    .exe + excel bestand in /bin/

---
+++ Testing Applications/ExcelTest/ExcelTest/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ExcelTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Excel.Range range;
+        private Excel.Worksheet worksheet;
+        private Excel.Workbook workbook;
+        private Excel.Application excelApp;
+        private static DataTable dt;
+        private Thread myThread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            excelApp = new Excel.Application();
+
+        }
+
+        public delegate void showData(DataTable dt);
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            Nullable<bool> result = o.ShowDialog();
+            if (result == true)
+            {
+                if (o.FileName.EndsWith(".xlsx") || o.FileName.EndsWith(".xls"))
+                {
+                    workbook = excelApp.Workbooks.Open(o.FileName);
+                    myThread = new Thread(() =>
+                    {
+                        getData();
+                    });
+                    myThread.Start();
+                    SplashWindow s = new SplashWindow(myThread);
+                    s.ShowDialog();
+                    this.DataContext = dt.DefaultView;
+
+                }
+            }
+        }
+
+        private void getData()
+        {
+
+            worksheet = (Excel.Worksheet)workbook.Sheets.get_Item(1);
+
+            int row = 0;
+            int column = 0;
+
+            range = worksheet.UsedRange;
+            dt = new DataTable();
+
+            for (row = 1; row <= range.Rows.Count; row++)
+            {
+                DataRow dr = null;
+                if (row == 1)
+                {
+
+                }
+                else
+                {
+                    dr = dt.NewRow();
+                }
+                for (column = 1; column <= range.Columns.Count; column++)
+                {
+                    if (row == 1)
+                    {
+                        if ((range.Cells[row, column] as Excel.Range).Value2 != null)
+                            dt.Columns.Add((range.Cells[row, column] as Excel.Range).Value2.ToString());
+                    }
+                    else
+                    {
+                        if ((range.Cells[row, column] as Excel.Range).Value2 != null)
+                        {
+                            dr[column - 1] = (range.Cells[row, column] as Excel.Range).Value2.ToString();
+                        }
+                    }
+                }
+                if (dr != null)
+                {
+                    dt.Rows.Add(dr);
+                }
+            }
+            workbook.Close(false, Missing.Value, Missing.Value);
+            excelApp.Quit();
+
+        }
+    }
+}
---
+++ Testing Applications/ExcelTest/ExcelTest/SplashWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ExcelTest
+{
+    /// <summary>
+    /// Interaction logic for SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+
+        Thread loadingThread;
+        Storyboard Showboard;
+        Storyboard Hideboard;
+        private delegate void ShowDelegate(string txt);
+        private delegate void HideDelegate();
+        ShowDelegate showDelegate;
+        HideDelegate hideDelegate;
+
+        Thread isLoading;
+
+        public SplashWindow(Thread t)
+        {
+            isLoading = t;
+            InitializeComponent();
+
+            showDelegate = new ShowDelegate(this.showText);
+            hideDelegate = new HideDelegate(this.hideText);
+
+
+            Showboard = this.Resources["showStoryBoard"] as Storyboard;
+            Hideboard = this.Resources["HideStoryBoard"] as Storyboard;
+
+            var da = new DoubleAnimation(360, 0, new Duration(TimeSpan.FromSeconds(5)));
+            var rt = new RotateTransform();
+            loadingLabel.RenderTransform = rt;
+            loadingLabel.RenderTransformOrigin = new Point(0.5, 0.5);
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            rt.BeginAnimation(RotateTransform.AngleProperty, da);
+
+            loadingThread = new Thread(load);
+            loadingThread.Start();
+        }
+
+        private void load()
+        {
+
+            //Thread.Sleep(1000);
+            this.Dispatcher.Invoke(showDelegate, "Importing data...");           
+            //Thread.Sleep(2000);
+            //do some loading work
+            while (isLoading.IsAlive)
+            {
+                
+            }
+            this.Dispatcher.Invoke(hideDelegate);
+
+            //close the window
+           // Thread.Sleep(2000);
+            this.Dispatcher.Invoke(DispatcherPriority.Normal,
+        (Action)delegate() { Close(); });
+        }
+
+        private void showText(string txt)
+        {
+            txtLoading.Text = txt;
+            BeginStoryboard(Showboard);
+        }
+        private void hideText()
+        {
+            BeginStoryboard(Hideboard);
+        }
+    }
+}
