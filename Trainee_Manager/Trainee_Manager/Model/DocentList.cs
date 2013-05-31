72a80cf53a26277de520aacf7e991bfa92055dc1
Gijs van der Venne <gijs.venne@hotmail.com>
2013-05-31 16:36:35 +0200

    shit geschizzled

---
+++ Trainee_Manager/Trainee_Manager/Model/DocentList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Manager.Model
+{
+    class DocentList
+    {
+         List<Docent> DocentenList;
+         List<Docent> SortedDocentList;
+
+         public DocentList()
+         {
+             DocentenList = new List<Docent>();
+         }
+
+        public void SortList()
+        {
+            SortedDocentList = DocentenList.OrderBy(o => o.Rating).ToList();
+        }
+
+    }
+}
---
+++ Trainee_Manager/Trainee_Manager/Model/RatingCalculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trainee_Manager.Controller;
+
+namespace Trainee_Manager.Model
+{
+    class RatingCalculator
+    {
+        private Docent docent;
+        private StageOpdracht opdracht;
+
+        private static DataTable dataTable;
+
+        public RatingCalculator()
+        {
+            getOpdracht();
+            getData();
+        }
+
+        private void getOpdracht()
+        {
+            opdracht = new StageOpdracht();
+            
+            //foreach student koppeld aan de opdracht
+
+            getDocentData();
+        }
+
+        private void getDocentData()
+        { 
+            //foreach docent die bestaat
+            docent = new Docent();
+
+        }
+
+        public void improveRating(String preference)
+        { 
+            switch(preference)
+            {
+
+                case "VakerBijBedrijfGeweest":
+                    docent.Rating += 10;
+                    break;
+
+                case "afstandtwee":
+                    docent.Rating += 15;
+                    break;
+
+                case "afstandeen":
+                    docent.Rating +=25;
+                    break;
+
+                case "voorkeur":
+                    docent.Rating += 50;
+                    break;
+
+                case "kennis":
+                    docent.Rating += 75;
+                    break;
+
+                case "vrijeuren":
+                    docent.Rating += 100;
+                    break;        
+
+                case "MeerdereStudentenBijHetzelfdeBedrijf":
+                    docent.Rating += 100;
+                    break;
+
+                case "PerseeStudent":
+                    docent.Rating += 1000;
+                    break;
+                
+                default:
+                    docent.Rating += 0;
+                    break;
+            }
+        }
+
+        //Call the procedure to load the mysql data
+        private void getData()
+        {
+            dataTable = DatabaseConnection.commandSelect("");
+        }
+    }
+}
