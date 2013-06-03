e93ba91191fb90a3e2ed75e274b31a068611077a
Gijs van der Venne <gijs.venne@hotmail.com>
2013-06-03 21:50:32 +0200

    nog een keer variable aangemaakt (@github??)

---
+++ Trainee_Manager/Trainee_Manager/Model/Bedrijf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Manager.Model
+{
+    class Bedrijf
+    {
+        private string _naam;
+        private string _plaats;
+        private string _postcode;
+        private string _straat;
+
+        public string Naam { get; set; }
+        public string Plaats { get; set; }
+        public string Postcode { get; set; }
+        public string Straat { get; set; }
+
+        public Bedrijf(string naam, string plaats, string postcode, string straat)
+        {
+            Naam = naam;
+            Plaats = plaats;
+            Postcode = postcode;
+            Straat = straat;
+        }
+
+    }
+}
---
+++ Trainee_Manager/Trainee_Manager/Model/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Manager.Model
+{
+    class Student
+    {
+        private int _studentid;
+        private int _studentnummer;
+
+        private string _naam;
+
+        public int Studentid { get; set; }
+        public int Studentnummer { get; set; }
+
+        public string Naam { get; set; }
+
+        public Student(int studentid, int studentnummer, String studentnaam)
+        {
+            Studentid = studentid;
+            Studentnummer = studentnummer;
+            Naam = studentnaam;
+        }
+
+
+
+    }
+}
