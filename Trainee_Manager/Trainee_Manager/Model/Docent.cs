478f0c1c7843da8d0af6b6966d6d031ed491e146
Gijs van der Venne <gijs.venne@hotmail.com>
2013-06-03 21:48:43 +0200

    Variable aangemaakt voor rating systeem

--- Trainee_Manager/Trainee_Manager/Model/Docent.cs
+++ Trainee_Manager/Trainee_Manager/Model/Docent.cs
@@ -8,9 +8,28 @@ namespace Trainee_Manager.Model
 {
     class Docent
     {
+        private string _naam;
+        private string _email;
+        private string _adres;
+        private string _postcode;
+
+        private int _vrije_uren;
+        private int _periode;
         private int _rating;
 
-        public int Rating { get; set; }
+        List<string> kenmerken;
+        List<string> VoorkeurBedrijven;
+
+        List<int> VoorkeurStages;
+
+        public string Naam { get; set; }
+        public string Email { get; set; }
+        public string Adres { get; set; }
+        public string Postcode { get; set; }
+
+        public int Vrije_uren   { get; set; }
+        public int Periode      { get; set; }
+        public int Rating       { get; set; }
 
 
     }
--- Trainee_Manager/Trainee_Manager/Model/StageOpdracht.cs
+++ Trainee_Manager/Trainee_Manager/Model/StageOpdracht.cs
@@ -10,9 +10,28 @@ namespace Trainee_Manager.Model
 {
     class StageOpdracht
     {
-        public StageOpdracht()
-        {
+        private Boolean _afstudeerOpdracht;
 
+        private int _stageID;
+        private int _periode;
+
+        private Bedrijf bedrijf;
+
+        private Student student1;
+        private Student student2;
+
+        List<string> kenmerken;
+
+        public int StageID { get; set; }
+        public int Periode { get; set; }
+
+        public StageOpdracht(Student student1, Student student2, Bedrijf bedrijf)
+        {
+            this.student1 = student1;
+            this.student2 = student2;
+            this.bedrijf = bedrijf;
+
+            kenmerken = new List<string>();
         }
     }
 }
