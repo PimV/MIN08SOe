0815b740f30fd3e7806c1344c54e6363b2d11c47
Devin van Nieuwenhoven <turbodevin@hotmail.com>
2013-06-06 13:57:54 +0200

    Kenmerk Beheer gemaakt.
    Eerste commit volgens mij :D

--- Trainee_Manager/Trainee_Manager/View/ConfigPage.xaml.cs
+++ Trainee_Manager/Trainee_Manager/View/ConfigPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Trainee_Manager.Controller;
 
 namespace Trainee_Manager.View
 {
@@ -23,6 +25,39 @@ namespace Trainee_Manager.View
         public ConfigPage()
         {
             InitializeComponent();
+            loadSubjects();
+        }
+
+        private void loadSubjects()
+        {
+            //Fill the listbox containing ALL subjects
+            DataTable tempTable = DatabaseConnection.commandSelect("SELECT * FROM kenmerken");
+            listbox_SubjectAll.SelectedValuePath = "id";
+            listbox_SubjectAll.DisplayMemberPath = "kenmerk";
+            listbox_SubjectAll.ItemsSource = tempTable.DefaultView;
+        }
+
+        private void button_SubjectNew_Click(object sender, RoutedEventArgs e)
+        {
+            if (newSubjectText.Text.Trim() != "")
+            {
+                DatabaseConnection.commandEdit("INSERT INTO kenmerken (kenmerk) SELECT * FROM (SELECT '" + newSubjectText.Text + "') AS tmp WHERE NOT EXISTS (SELECT * FROM kenmerken WHERE kenmerk = '" + newSubjectText.Text + "');");
+                loadSubjects();
+            }
+            newSubjectText.Clear();
+        }
+
+        private void button_subjectDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (listbox_SubjectAll.SelectedItem != null)
+            {
+                DataRowView selection = (DataRowView)listbox_SubjectAll.SelectedItem;
+                int id = Convert.ToInt32(selection.Row["id"]);
+
+
+                DataTable tempTable = DatabaseConnection.commandSelect("CALL procedure_kenmerken_delete(" + id + ");");
+                loadSubjects();
+            }
         }
     }
 }
